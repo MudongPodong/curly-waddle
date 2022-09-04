45a2e46f696f5f80087500ddeb8d69017e06014a
MudongPodong <108624107+MudongPodong@users.noreply.github.com>
2022-09-04 23:09:37 +0900

    Rename Optimal.java to 페이지교체정책/Optimal.java

--- 페이지교체정책/Optimal.java
+++ 페이지교체정책/Optimal.java
@@ -16,15 +16,15 @@ public class Optimal extends JFrame {
 	
 	JLabel str1=new JLabel("Reference String");
 	JLabel str2=new JLabel("#Frame");
-	JLabel hitstr1=new JLabel("HitÈ½¼ö:");
-	JLabel pfstr1=new JLabel("PageFaultÈ½¼ö:");//51877415123519968451ABC15
-	JLabel migratestr1=new JLabel("MigrateÈ½¼ö:");
+	JLabel hitstr1=new JLabel("HitíšŸìˆ˜:");
+	JLabel pfstr1=new JLabel("PageFaultíšŸìˆ˜:");//51877415123519968451ABC15
+	JLabel migratestr1=new JLabel("MigrateíšŸìˆ˜:");
 	JLabel hitstr2=new JLabel();
 	JLabel pfstr2=new JLabel();
 	JLabel migratestr2=new JLabel();
 	JLabel pfrate1=new JLabel("Page Fault Rate(%):");
 	JLabel pfrate2=new JLabel();
-	JLabel runtime=new JLabel("½ÇÇà½Ã°£:");
+	JLabel runtime=new JLabel("ì‹¤í–‰ì‹œê°„:");
 	JLabel runtime2=new JLabel();
 	
 	JTextField str11=new JTextField();
@@ -84,12 +84,12 @@ public class Optimal extends JFrame {
 				
 				for(int i=0;i<frame.length;i++) {
 					frame[i]=new Frame();
-					frame[i].setstr("-");   //!´Â ºñ¾ú´Ù´Â ¶æ
+					frame[i].setstr("-");   //!ëŠ” ë¹„ì—ˆë‹¤ëŠ” ëœ»
 					frame[i].setorder(i);
 				}
 				
 				targetstr=str11.getText().split("");
-				for(int i=0;i<targetstr.length;i++) {   //¹®ÀÚ¿­ µ¹·Áº¸±â
+				for(int i=0;i<targetstr.length;i++) {   //ë¬¸ìžì—´ ëŒë ¤ë³´ê¸°
 					point=0;
 					for(int j=0;j<framenum;j++) {       //hit appear
 						if(targetstr[i].equals(frame[j].getstr())) {
@@ -101,7 +101,7 @@ public class Optimal extends JFrame {
 						}
 					}
 					if(point==0) {
-						for(int j=0;j<framenum;j++) {    //hit¿¡ ½ÇÆÐÇÏ°í ¾ÆÁ÷ ºó°ø°£ÀÌ ÀÖÀ»¶§
+						for(int j=0;j<framenum;j++) {    //hitì— ì‹¤íŒ¨í•˜ê³  ì•„ì§ ë¹ˆê³µê°„ì´ ìžˆì„ë•Œ
 							if(frame[j].getstr().equals("-")) {    
 								frame[j].setstr(targetstr[i]);
 								frame[j].setorder(0);
